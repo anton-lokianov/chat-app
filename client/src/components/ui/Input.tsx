f421adb958c80ca0cc4fe636649869af1f5ee83c
anton lokianov <antonlokianov@gmail.com>
2023-12-29 14:02:37 +0200

    Refactor Input component to remove unused code

--- client/src/components/ui/Input.tsx
+++ client/src/components/ui/Input.tsx
@@ -1,37 +1,12 @@
 import React from 'react';
-import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/cn';
 
-const inputVariants = cva('some-class', {
-  variants: {
-    variant: {
-      default: 'bg-gray-500',
-      primary: 'bg-blue-500',
-      secondary: 'bg-gray-500',
-    },
-    size: {
-      default: 'text-sm',
-      medium: 'text-md',
-      large: 'text-lg',
-    },
-  },
-  defaultVariants: {
-    variant: 'default',
-    size: 'default',
-  },
-});
-
-type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
-  VariantProps<typeof inputVariants> & {};
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {};
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, ...props }, ref) => {
     return (
-      <input
-        ref={ref}
-        {...props}
-        className={cn(inputVariants({ className, variant, size }))}
-      >
+      <input ref={ref} {...props} className={cn(`${className}`)}>
         Input
       </input>
     );
