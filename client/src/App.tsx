029e0a3a53848d586af1af88ac39511fceca0543
anton lokianov <antonlokianov@gmail.com>
2023-12-29 12:38:54 +0200

    Add @types directory to resolve path aliases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <></>;
+  return <main></main>;
 }
 
 export default App;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       "@utils": path.resolve(__dirname, "./src/utils"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@services": path.resolve(__dirname, "./src/services"),
+      "@types": path.resolve(__dirname, "./src/types"),
     },
   },
   plugins: [react()],
