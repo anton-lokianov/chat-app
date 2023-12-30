736e8db4b122ec4a4ae4a7ba6209e3b7bb34739e
anton-lokinanov <antonlokianov@gmail.com>
2023-12-30 22:38:43 +0200

    Refactor sign-in form and update layout

--- client/src/components/features/SignInForm.tsx
+++ client/src/components/features/SignInForm.tsx
@@ -19,13 +19,11 @@ export const SignInForm = () => {
     },
   });
 
-  // todo: add onSubmit handler add more styles
-  // BA36A6 hax color for the after element
   return (
     <Form {...form}>
       <form
         onSubmit={() => {}}
-        className='space-y-5 max-w-md w-full relative bg-[#FEFEF8] p-9 rounded-sm shadow-md shadow-black border border-gray-200'
+        className='space-y-6 h-screen w-full relative bg-[#FEFEF8] p-9 rounded-sm shadow-md shadow-black border border-gray-200 form'
       >
         <div className='flex justify-center items-center text-slate-800 font-bold text-[1.3rem]'>
           <span>Sign In</span>
@@ -57,19 +55,21 @@ export const SignInForm = () => {
             </FormItem>
           )}
         />
-        <Button
-          className='w-full tracking-widest'
-          variant='default'
-          type='submit'
-          size='sm'
-        >
-          SIGN IN
-        </Button>
-        <div className='text-slate-500'>
-          dont have an account?{' '}
-          <Button variant='link' type='button'>
-            register
+        <div>
+          <Button
+            className='w-full tracking-widest'
+            variant='default'
+            type='submit'
+            size='sm'
+          >
+            SIGN IN
           </Button>
+          <div className='text-slate-500'>
+            dont have an account?{' '}
+            <Button variant='link' type='button'>
+              register
+            </Button>
+          </div>
         </div>
       </form>
     </Form>
